5e0129df3e501c0875084f317be85daf12b59a94
dhmarino <diego.marino@valid.com>
2024-08-30 18:33:40 -0300

    Commit Inicial

---
+++ GeneracionBureau/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace GeneracionBureau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.BtnHerramientas = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(24, 170);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1521, 771);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GeneracionBureau.Properties.Resources.Valid_logo_black;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 23);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(599, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Image = ((System.Drawing.Image)(resources.GetObject("BtnSalir.Image")));
+            this.BtnSalir.Location = new System.Drawing.Point(24, 963);
+            this.BtnSalir.Margin = new System.Windows.Forms.Padding(6);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(100, 96);
+            this.BtnSalir.TabIndex = 26;
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // BtnHerramientas
+            // 
+            this.BtnHerramientas.Image = ((System.Drawing.Image)(resources.GetObject("BtnHerramientas.Image")));
+            this.BtnHerramientas.Location = new System.Drawing.Point(136, 963);
+            this.BtnHerramientas.Margin = new System.Windows.Forms.Padding(6);
+            this.BtnHerramientas.Name = "BtnHerramientas";
+            this.BtnHerramientas.Size = new System.Drawing.Size(100, 96);
+            this.BtnHerramientas.TabIndex = 27;
+            this.BtnHerramientas.UseVisualStyleBackColor = true;
+            this.BtnHerramientas.Click += new System.EventHandler(this.BtnHerramientas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1580, 1080);
+            this.Controls.Add(this.BtnHerramientas);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generación Bureau";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Button BtnHerramientas;
+    }
+}
+
---
+++ GeneracionBureau/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneracionBureau //Botones Dinamicos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LeerArchivo();
+            GenerarBotones();
+        }
+        string rutaArchivo = "C:\\valid\\clientes\\BUREAU\\clientes.ini"; // Reemplaza con la ruta completa de tu archivo
+        List<string[]> datos = new List<string[]>();
+        private void LeerArchivo()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(rutaArchivo))
+                {
+                    string linea;
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        string[] datosLinea = linea.Split(','); // Ajusta el delimitador si es diferente
+                        datos.Add(datosLinea);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void GenerarBotones()
+        {// Limpiar el panel antes de agregar nuevos botones
+            panel1.Controls.Clear();
+            
+            // Ordenar los datos por el nombre (primer elemento de cada línea)
+            var datosOrdenados = datos.OrderBy(r => r[0]).ToList();
+
+            int x = 10; // Posición inicial en X
+            int y = 10; // Posición inicial en Y
+            int anchoBoton = 180;
+            int altoBoton = 40;
+            int espacioEntreBotones = 10;
+            int columnaActual = 0;
+            int maxColumnas = panel1.Width / (anchoBoton + espacioEntreBotones); // Calcula el número máximo de columnas
+            try
+            {
+                foreach (string[] datosLinea in datosOrdenados)
+                {
+                    Button btn = new Button();
+                    btn.Text = datosLinea[0];
+                    btn.Location = new Point(x, y);
+                    btn.Size = new Size(anchoBoton, altoBoton);
+                    btn.Image = Image.FromFile(datosLinea[2]);
+                    btn.ImageAlign = ContentAlignment.MiddleLeft;
+                    btn.TextAlign = ContentAlignment.MiddleRight;
+                    //btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.BorderSize = 0;
+                    btn.Font = new Font("Arial", 12, FontStyle.Bold);
+
+                    if (columnaActual >= maxColumnas)
+                    {
+                        // Si se llegó al final de la fila, reiniciar la columna y aumentar la fila
+                        columnaActual = 0;
+                        y += altoBoton + espacioEntreBotones;
+                    }
+                    x = columnaActual * (anchoBoton + espacioEntreBotones) + 10;
+
+                    btn.Location = new Point(x, y);
+                    panel1.Controls.Add(btn);
+
+                    columnaActual++;
+
+                    // Manejar posibles excepciones al ejecutar el programa
+                    btn.Click += (sender, e) =>
+                    {
+                        try
+                        {
+                            System.Diagnostics.Process.Start(datosLinea[1]);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error al ejecutar el programa: " + ex.Message);
+                        }
+                    };
+
+                    panel1.Controls.Add(btn);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            // Forzar una actualización de la interfaz del panel
+            panel1.Refresh();
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnHerramientas_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cliente = "HBUREAU";
+                StreamReader config = new StreamReader(cliente + ".ini", false);
+                config.ReadLine();
+                string inputdir = config.ReadLine().Trim();
+                config.ReadLine();
+                string nombre_ejecutable = config.ReadLine().Trim();
+
+                this.Enabled = false;
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.WorkingDirectory = @inputdir;
+                psi.FileName = @nombre_ejecutable;
+                psi.CreateNoWindow = true;
+                Process p = Process.Start(psi);
+                p.WaitForExit();
+                this.Activate();
+                this.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error..." + ex.Message);
+                this.Enabled = true;
+            }
+        }
+    }
+}
