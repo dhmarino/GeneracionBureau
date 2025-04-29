e9645782c0cc3036df94d35640ef4b53ce319f6b
dhmarino <diego.marino@valid.com>
2025-04-29 14:35:47 -0300

    Se agranda el tamaño del formulario Form1 en 60 para que entren mas botones

--- GeneracionBureau/Form1.Designer.cs
+++ GeneracionBureau/Form1.Designer.cs
@@ -39,19 +39,17 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(24, 169);
-            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Location = new System.Drawing.Point(12, 88);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1520, 771);
+            this.panel1.Size = new System.Drawing.Size(760, 460);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::GeneracionBureau.Properties.Resources.Valid_logo_black;
-            this.pictureBox1.Location = new System.Drawing.Point(24, 23);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(600, 135);
+            this.pictureBox1.Size = new System.Drawing.Size(300, 70);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -59,10 +57,9 @@
             // BtnSalir
             // 
             this.BtnSalir.Image = ((System.Drawing.Image)(resources.GetObject("BtnSalir.Image")));
-            this.BtnSalir.Location = new System.Drawing.Point(24, 963);
-            this.BtnSalir.Margin = new System.Windows.Forms.Padding(6);
+            this.BtnSalir.Location = new System.Drawing.Point(12, 558);
             this.BtnSalir.Name = "BtnSalir";
-            this.BtnSalir.Size = new System.Drawing.Size(100, 96);
+            this.BtnSalir.Size = new System.Drawing.Size(50, 50);
             this.BtnSalir.TabIndex = 26;
             this.BtnSalir.UseVisualStyleBackColor = true;
             this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
@@ -70,10 +67,9 @@
             // BtnHerramientas
             // 
             this.BtnHerramientas.Image = ((System.Drawing.Image)(resources.GetObject("BtnHerramientas.Image")));
-            this.BtnHerramientas.Location = new System.Drawing.Point(136, 963);
-            this.BtnHerramientas.Margin = new System.Windows.Forms.Padding(6);
+            this.BtnHerramientas.Location = new System.Drawing.Point(68, 558);
             this.BtnHerramientas.Name = "BtnHerramientas";
-            this.BtnHerramientas.Size = new System.Drawing.Size(100, 96);
+            this.BtnHerramientas.Size = new System.Drawing.Size(50, 50);
             this.BtnHerramientas.TabIndex = 27;
             this.BtnHerramientas.UseVisualStyleBackColor = true;
             this.BtnHerramientas.Click += new System.EventHandler(this.BtnHerramientas_Click);
@@ -82,17 +78,18 @@
             // 
             this.lblVersion.AutoSize = true;
             this.lblVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblVersion.Location = new System.Drawing.Point(633, 133);
+            this.lblVersion.Location = new System.Drawing.Point(316, 69);
+            this.lblVersion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(92, 25);
+            this.lblVersion.Size = new System.Drawing.Size(49, 13);
             this.lblVersion.TabIndex = 28;
             this.lblVersion.Text = "Version";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1580, 1081);
+            this.ClientSize = new System.Drawing.Size(790, 620);
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.BtnHerramientas);
             this.Controls.Add(this.BtnSalir);
@@ -100,7 +97,6 @@
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Generación Bureau";
